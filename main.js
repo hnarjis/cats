61b157a7faf0c1e044bc232f6ade456214363892
Narjis Hachilif <hnarjis@gmail.com>
2014-11-15 19:53:41 +0100

    Resizing canvas to fit browser size

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
 	canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+	canvas.height = window.innerHeight;
 	var ctx = canvas.getContext('2d');
 	
 	ctx.fillStyle = '#708090';
