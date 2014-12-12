67f9f7523324a870fe9915838511bb594179c0fb
Narjis Hachilif <hnarjis@gmail.com>
2014-12-12 23:35:14 +0100

    Updated canvas

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
 window.onload = function() {
 	var canvas = document.getElementById('canvas');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
 	var ctx = canvas.getContext('2d');
-	
-	ctx.fillStyle = '#708090';
-	ctx.fillRect(window.innerWidth-1000, 0, 1000, window.innerHeight);
-
-	ctx.font = "20px Arial";
-	ctx.fillStyle = "white";
-	ctx.fillText("Welcome.",15,40);
+    canvas.height = canvas.parentNode.clientWidth * 0.75 * (canvas.height/canvas.width);  
+    canvas.width = canvas.parentNode.clientWidth * 0.75;
 }
