605c7b11f021446d32a34b28b1f56f482b7e1d88
Narjis Hachilif <hnarjis@gmail.com>
2015-01-04 00:30:19 +0100

    Moving in random positions

--- main.js
+++ main.js
@@ -1,52 +1,65 @@
 var GAME = {};
 
-var Game = function() {
-	var context;
-	var timeout = 400;
+GAME.cats = function() {
+    var context;
+    var time;
 
     function init() {
-    	var canvas = document.getElementById('canvas');
-	    canvas.height = canvas.parentNode.clientWidth * 0.75 * (canvas.height/canvas.width);  
-	    canvas.width = canvas.parentNode.clientWidth * 0.75;
-	    context = canvas.getContext('2d');
-	    loop();
+        var canvas = document.getElementById('canvas');
+        canvas.height = canvas.parentNode.clientWidth * 0.75 * (canvas.height/canvas.width);  
+        canvas.width = canvas.parentNode.clientWidth * 0.75;
+        context = canvas.getContext('2d');
+        time = 0;
+        loop(0);
     }
 
-    function loop() {
-		context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-	    draw();
-	    setTimeout(loop, timeout);
-	}
+    function loop(now) {
+        if (now - time > 400) {
+            context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+            draw('white',randomizePosition());
+            draw('red',randomizePosition());
+            time = now; 
+        }
+        window.requestAnimationFrame(loop);
+    }
 
-	function draw() {
-		context.strokeStyle = 'white';
-    	context.fillStyle = 'white';
+    function randomizePosition() {
+        var gridWidth = context.canvas.width;
+        var gridHeight = context.canvas.height;
+        var x = 50 + Math.round((gridWidth-100) * Math.random());
+        var y = 50 + Math.round((gridHeight-100) * Math.random());
+        return [x, y];
+    }
 
-    	// Face
-	    context.beginPath();
-		context.arc(700, 300, 50, 0, Math.PI * 2);
-		context.stroke();
+    function draw(color, position) {
+        context.strokeStyle = color;
+        context.fillStyle = color;
 
-		// Left ear
-		context.beginPath();
-		context.lineTo(700 + 50 * Math.cos(-150 * (Math.PI/180)), 300 + 50 * Math.sin(-150 * (Math.PI/180)));
-		context.lineTo(700 + 70 * Math.cos(-135 * (Math.PI/180)), 300 + 70 * Math.sin(-135 * (Math.PI/180)));
-		context.lineTo(700 + 50 * Math.cos(-120 * (Math.PI/180)), 300 + 50 * Math.sin(-120 * (Math.PI/180)));
-		context.stroke();
+        // Face
+        context.beginPath();
+        context.arc(position[0], position[1], 50, 0, Math.PI * 2);
+        context.stroke();
 
-		// Right ear
-		context.beginPath();
-		context.lineTo(700 + 50 * Math.cos(-60 * (Math.PI/180)), 300 + 50 * Math.sin(-60 * (Math.PI/180)));
-		context.lineTo(700 + 70 * Math.cos(-45 * (Math.PI/180)), 300 + 70 * Math.sin(-45 * (Math.PI/180)));
-		context.lineTo(700 + 50 * Math.cos(-30 * (Math.PI/180)), 300 + 50 * Math.sin(-30 * (Math.PI/180)));
-		context.stroke();
-	}
+        // Left ear
+        context.beginPath();
+        context.lineTo(position[0] + 50 * Math.cos(-150 * (Math.PI/180)), position[1] + 50 * Math.sin(-150 * (Math.PI/180)));
+        context.lineTo(position[0] + 70 * Math.cos(-135 * (Math.PI/180)), position[1] + 70 * Math.sin(-135 * (Math.PI/180)));
+        context.lineTo(position[0] + 50 * Math.cos(-120 * (Math.PI/180)), position[1] + 50 * Math.sin(-120 * (Math.PI/180)));
+        context.stroke();
 
-	return {
-	    init: init
-	};
+        // Right ear
+        context.beginPath();
+        context.lineTo(position[0] + 50 * Math.cos(-60 * (Math.PI/180)), position[1] + 50 * Math.sin(-60 * (Math.PI/180)));
+        context.lineTo(position[0] + 70 * Math.cos(-45 * (Math.PI/180)), position[1] + 70 * Math.sin(-45 * (Math.PI/180)));
+        context.lineTo(position[0] + 50 * Math.cos(-30 * (Math.PI/180)), position[1] + 50 * Math.sin(-30 * (Math.PI/180)));
+        context.stroke();
+    }
+
+    return {
+        init: init
+    };
 }();
 
 window.onload = function() {
-  	Game.init();
+    GAME.cats.init();
 }
